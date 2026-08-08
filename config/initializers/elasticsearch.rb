822ba8a0f7d638cd26c36fcc6323a1c4bb154972
Naveen Thonpunoori <naveenthonpunoori@gmail.com>
2026-08-08 16:26:42 +0530

    Removing elastic search

--- config/initializers/elasticsearch.rb
+++
@@ -1,6 +0,0 @@
-if defined?(Elasticsearch)
-  Elasticsearch::Model.client = Elasticsearch::Client.new(
-    url: ENV.fetch("ELASTICSEARCH_URL", "http://localhost:9200"),
-    log: Rails.env.development?
-  )
-end

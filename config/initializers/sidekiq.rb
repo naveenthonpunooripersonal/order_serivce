9b6375e51f3e48a245c51f8175c2f5f0939b2c27
Naveen Thonpunoori <naveenthonpunoori@gmail.com>
2026-08-08 18:18:17 +0530

    removing redis from here

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,8 @@
 redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/0")
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
-end
-
+# Configure order_service exclusively as a job producer (Client)
 Sidekiq.configure_client do |config|
   config.redis = { url: redis_url }
 end
+
+# Server configuration removed since Sidekiq worker lives in the worker microservice

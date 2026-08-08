9c6d3dd4aa5c9b7e578ce29ee58c7d1226d2183e
Naveen Thonpunoori <naveenthonpunoori@gmail.com>
2026-08-08 21:15:28 +0530

    Minor changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1
-ARG RUBY_VERSION=3.3.12
+ARG RUBY_VERSION=3.3.1
 FROM ruby:$RUBY_VERSION-slim AS base
 
 WORKDIR /app
--- config/initializers/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/0")
-
-# Configure order_service exclusively as a job producer (Client)
-Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
-end
-
-# Server configuration removed since Sidekiq worker lives in the worker microservice
